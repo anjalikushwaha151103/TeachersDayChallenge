16b956ede76464273273a23ed90765b7ef621164
anjalikushwaha151103 <anjalikushwaha8009@gmail.com>
2025-08-26 12:46:18 +0530

    Aug 26th

---
+++ Aug26th/problem2.cpp
@@ -0,0 +1,23 @@
+// Divisibility Problem (Problem 1328A)
+// Statement: For a given number, determine if and how it can be divided or analyzed for divisibility with specific conditions.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for (int i=0;i<n;i++){
+        int a;
+        int b;
+        cin>>a>>b;
+        int res;
+        
+        if(a%b==0){
+            res=0;
+        }else{
+            res=b-(a%b);
+        }
+        cout<<res<<endl;
+    }
+}
